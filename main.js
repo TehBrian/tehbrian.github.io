8405f07dd4e13891f95e80445837ce42f1c05229
t3hbrian <tehbrian@outlook.com>
2018-03-31 17:26:34 -0400

    Added use to the buttons! Still useless though.

---
+++ main.js
@@ -0,0 +1,7 @@
+function buttonOne() {
+    document.getElementById("one").innerHTML = "Clicked One Button!";
+}
+
+function buttonTwo() {
+    document.getElementById("two").innerHTML = "Clicked Two Button!";
+}
