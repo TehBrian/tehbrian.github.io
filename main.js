d51ca8e31cb711004afd9829b1c4a1c6e50266f7
t3hbrian <tehbrian@outlook.com>
2018-03-31 17:50:17 -0400

    Added alerts to buttons

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 function buttonOne() {
     document.getElementById("one").innerHTML = "Clicked One Button!";
+    window.alert("Clicked On Button!");
 }
 
 function buttonTwo() {
     document.getElementById("two").innerHTML = "Clicked Two Button!";
+    window.alert("Clicked Two Button!");
 }
