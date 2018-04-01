bbfebdaf5a44f130210d90b904b0693a26bea6c2
t3hbrian <tehbrian@outlook.com>
2018-03-31 22:32:35 -0400

    Added a random function and a use for it. Testing out Javascript.

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function buttonOne() {
     document.getElementById("one").innerHTML = "Clicked One Button!";
-    window.alert("Clicked On Button!");
+    window.alert("Clicked One Button!");
 }
 
 function buttonTwo() {
-    document.getElementById("two").innerHTML = "Clicked Two Button!";
-    window.alert("Clicked Two Button!");
+    document.getElementById("two").innerHTML = "Clicked Two Button! And, your random number is " + randomNum(0, 10) + "!";
 }
