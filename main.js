935193417d246df05a5d1b8f16dc897e1074b470
t3hbrian <tehbrian@outlook.com>
2018-04-11 21:22:51 -0400

    Made the site a fun number generator! :)

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
 function randomNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-function buttonOne() {
-    document.getElementById("one").innerHTML = "Clicked One Button!";
-    window.alert("Clicked One Button!");
-}
-
-function buttonTwo() {
-    document.getElementById("two").innerHTML = "Clicked Two Button! And, your random number is " + randomNum(0, 10) + "!";
-}
